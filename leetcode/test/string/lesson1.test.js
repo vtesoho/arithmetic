94ef8eabf607581532f4b86c39a811deafb19686
vtesoho <vtesoho@163.com>
2019-08-07 23:44:03 +0800

    reverse string

---
+++ leetcode/test/string/lesson1.test.js
@@ -0,0 +1,5 @@
+import revertByWOrld from '../../code/string/lesson1'
+
+test('revertByWOrld:Let\'s take LeetCode contest',() => {
+  expect(revertByWOrld("Let's take LeetCode contest")).toBe("s'teL ekat edoCteeL tsetnoc")
+})
