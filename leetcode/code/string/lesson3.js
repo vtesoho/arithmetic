835dfa99f932fb64032c89b56c9f85b92d3cc4a9
vtesoho <vtesoho@163.com>
2019-08-10 23:19:36 +0800

    lesson3

---
+++ leetcode/code/string/lesson3.js
@@ -0,0 +1,23 @@
+// export default (str) => {
+//   for(i=0;i=str.length-1;i++){
+//     r=match(str.slice(i))
+//     console.log(r)
+//   }
+//   return 1
+  
+// }
+
+function test(str) {
+  let r
+  for(let i=0;i=str.length-1;i++){
+    r = matcha(str.slice(i))
+    console.log(r)
+  }
+}
+function matcha(str){
+  for (let i = 0; i < str.length; i++) {
+    if(str[0] == 0)
+    
+  }
+}
+test('00110011')
