ff89dfeb71ca7994ec67f406913fe4fdd7f805c8
vtesoho <vtesoho@163.com>
2019-12-23 22:15:21 +0800

    'hasGroupsSizeX'

--- leetcode/code/array/lesson2.js
+++ leetcode/code/array/lesson2.js
@@ -5,13 +5,28 @@ export default (arr) =>{
   if(arr.length % 2 == 1 || arr.length <= 1){
     return false
   }
+  let maxGroupItemNum = 0
+  let currenGroupItemNum = 0
   arr.sort(function (a,b) {return a-b})
   for (let i = 0; i < arr.length; i++) {
-    if(arr[i] != arr[i+1]){
+    if(i > 1){
+      if(arr[i] == arr[i-1]){
+        maxGroupItemNum++
+        currenGroupItemNum++
+      }else{
+        maxGroupItemNum = 0
+      }
+    }
+
+    if(arr[i] != arr[i+1] && maxGroupItemNum > currenGroupItemNum){
       return false
     }
+    
+   
     if(arr[i] == arr[i+1]){
       i++
+      currenGroupItemNum = currenGroupItemNum+2
+      maxGroupItemNum = maxGroupItemNum+2
     }
   }
   return true
